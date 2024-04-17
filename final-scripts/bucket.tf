1d1ce6da51ff4b3f181f16ad19d28bf6660b7a73
Shakthi <63875574+Lokak07@users.noreply.github.com>
2024-04-17 20:34:51 +0530

    Update bucket.tf

--- final-scripts/bucket.tf
+++ final-scripts/bucket.tf
@@ -1,19 +1,19 @@
 
 
-resource "aws_s3_bucket" "my-tf-backend-s3" {
-  bucket = "my-tf-backend-s4"
+resource "aws_s3_bucket" "my-tf-backend-s6" {
+  bucket = "my-tf-backend-s6"
   force_destroy = true
 
   tags = {
-    Name        = "My-terraform-backend-s4"
+    Name        = "My-terraform-backend-s6"
     Environment = "Dev"
   }
   
 }
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = "my-tf-backend-s4"
+  bucket = "my-tf-backend-s6"
   versioning_configuration {
     status = "Enabled"
   }
-}
+} 
